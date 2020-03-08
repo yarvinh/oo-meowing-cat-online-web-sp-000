dce3d3929ccaab8213afe9e8d6566cd14a59d0af
binding-archive-7591 <yarhumher@hotmail.com>
2020-03-08 00:44:25 +0000

    Done.

--- lib/meowing_cat.rb
+++ lib/meowing_cat.rb
@@ -1,1 +1,11 @@
-## code your solution here. 
+## code your solution here.
+class Cat
+    attr_reader :name
+    attr_writer :name
+    attr_accessor  :meow
+
+    def meow
+      puts "meow!"
+    end
+
+end
