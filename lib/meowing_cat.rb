d9d6d22007818de672dc43d84df3cd798e0c1c2f
binding-archive-7591 <yarhumher@hotmail.com>
2020-03-08 00:39:51 +0000

    Automatically backed up by Learn

--- lib/meowing_cat.rb
+++ lib/meowing_cat.rb
@@ -1,8 +1,8 @@
 ## code your solution here.
 class Cat
-    attr_reader :name
-    attr_writer :name
-    attr_accessor  :meow
-    @meow
+    attr_reader :name,:meow
+    attr_writer :name,:meow
+    # attr_accessor  :meow
+    puts @meow
 
 end
